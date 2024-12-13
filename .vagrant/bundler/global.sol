224d1d70867d45292a454435c50d349041f0e13b
Kuchař Martin <kuchar.martin@student.sposdk.cz>
2024-12-13 09:32:54 +0100

    Druhý pokus

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["ffi",["~> 1.0"]],["virtualbox",["= 0.8.6"]]],"checksum":"f84a0834d53f4443208ae29ac00eab27358cea94f1878d59a30f364dfd96bcd0","vagrant_version":"2.4.1"}
